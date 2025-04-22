29178089579d4e971a2eee2b9af625f606d05277
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2025-04-22 13:31:02 -0300

    feat: add image.

---
+++ data/info.js
@@ -0,0 +1,19 @@
+export const memes = [
+    {
+        id: 1,
+        title: "Diários de um Vampiro",
+        image: "https://blogs.correiobraziliense.com.br/proximocapitulo/wp-content/uploads/sites/27/2018/06/di%C3%A1rios-de-um-vampiro.jpg",
+    },
+
+    {
+        id: 2,
+        title: "Diários de um Vampiro",
+        image: "https://wallpapers.com/images/hd/vampire-diaries-dark-theme-io3s7516di18yqip.jpg",
+    },
+
+    {
+        id: 3,
+        title: "Diários de um Vampiro",
+        image: "https://wallpapers.com/images/hd/the-vampire-diaries-characters-in-a-forest-pltom0ahi8qd97c7.jpg",
+    },
+]
